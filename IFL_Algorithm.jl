f92e3a81c001e4c877f7b87a58c96b8f40235834
AngeloMilfont <amilfont@gmail.com>
2025-07-19 18:32:15 -0300

    Add IFL algorithm and supporting functions

---
+++ IFL_Algorithm.jl
@@ -0,0 +1,274 @@
+#
+#  IFL Algorithm
+#
+#  last update: AM 17/07/25
+#
+
+using LinearAlgebra
+using GLMNet
+
+# Supporting Function
+include("supporting_functions.jl")
+
+#
+# IFL
+#
+"""
+IFL(y, x, treat_outlier = true, intercept = true)
+
+Intercept = true => There is an intercept in x, then once the data is centralized, 
+  it can be removed from the first column of X (zeros) 
+
+Returns β_np_1 = v_beta_Par , β_n_p = m_beta_Par,                 # Solutions without Outliers
+        βo_np_1 = v_beta_Paro , βo_n_p = m_beta_Paro,             # Solutions with Outliers
+        β0 = β0_hat,                                              # Estimated Intercept
+        y_hat =  y_original_hat ,                                 # Insample Estimators without Outliers
+        y_hato = y_original_hat_woutl,                            # Insample Estimators with Outliers
+        mean_x  = mean_x , mean_y  = mean_y,                      # column-wise mean of X and mean of y
+        bic_value = bic_value,                                    # BIC value model without outliers
+        bic_value_outliers = bic_value_outliers,                  # BIC value model with outliers
+        chosen_var = chosen_variables,                            # List of selected columns in X
+        chosen_var_bk = chosen_variables_break,                   # List of selected columns with estim breaks in X
+        chosen_var_outliers = chosen_variables_outliers,          # List of selected columns in X (model w/outliers)
+        chosen_var_outliers_bk = chosen_variables_outliers_break, # List of selected columns with estim breaks in X (model w/outliers)
+        RMSE_inS = RMSE_inS, RMSE_inSo = RMSE_inSo,               # In Sample RMSE (models with and without outliers)
+        ind_out = ind_out,                                        # list of outlier in y  
+        IFL_t0 = IFL_t0                                           # time to converge
+
+Last review: 19 Jul 25
+
+"""
+function IFL(y, x, treat_outlier = true, intercept = true)
+  """
+  
+  """
+  
+  # Store originals
+  x_original = x
+  y_original = y
+
+  # Always center the data
+  mean_x = mean(x_original, dims=1)
+  mean_y = mean(y_original)
+  β0 = mean_y
+  y = y_original .- mean_y
+  x = x_original .- mean_x
+
+  # No need to process intercept column that equals 0!
+  if(intercept) 
+      x = x[:,2:end]  
+  end
+
+  ind_out=[]
+  
+  ########################################################
+  # Problem Dimensions
+  n = length(y) 
+  p = size(x)[2]
+  ########################################################
+  
+  global XX = Matrix{Float64}(undef, 0, 0)  # Alterei de Int para Float64!!! 27/05/25
+  for i in 1:p
+      if (size(XX, 1) == 0)
+          XX = Diagonal(x[:, i])  # Initialize with the first diagonal matrix
+      else
+          XX = hcat(XX, Diagonal(x[:, i]))  # Horizontally concatenate diagonal matrices
+      end
+  end
+  
+  #
+  # Setup Iteractive Fused LASSO 
+  #
+
+  nbetas_In  = repeat([n], p)
+  nbetas_In_Tot =sum(nbetas_In)
+  nbetas_Out = repeat([0], p)
+  nbetas_Out_Tot = sum(nbetas_Out)
+
+  W = I(n*p)
+
+  # Main loop
+  IFL_t0 = @elapsed begin 
+    Ld = L_Matrix(1, Int.(nbetas_In))
+    Ld_i = Inverse_L(Int.(nbetas_In))
+    
+    # Update model
+    H = XX * W * Ld_i
+    # Ada LASSO solution with smallest BIC
+    # Intercept is managed outside glmnet, therefore intercept here is always FALSE
+    theta_hat_Est = Ada_LASSO_intercept(H, y, false, false)
+    theta_hat = theta_hat_Est.coef
+    
+    #
+    # Gamma_d
+    #
+    Gamma_d = abs.(theta_hat) .> 0
+    #Gamma_d_Acc = cumsum(Gamma_d) 
+      
+    # Checking for 1s in free betas
+    #   Case when  d is larger than the number of diff values in p
+     
+    aux1 = 0
+    nbetas_In_Acc = cumsum(nbetas_In)
+    for i in 1:p
+      aux2 = aux1 + min(1,nbetas_In[i]) 
+      Gamma_d[(aux1+1):aux2] .= 1
+      aux1 = nbetas_In_Acc[i]
+    end
+      
+    #
+    # Calculating qty of diff betas in the next iteration
+    #
+    nbetas_Out = zeros(p)
+    nbetas_Out[1] = sum(Gamma_d[1:nbetas_In_Acc[1]])
+    for j in 2:p
+      aux1 = nbetas_In_Acc[j-1] + 1
+      aux2 = nbetas_In_Acc[j]
+      nbetas_Out[j] = sum(Gamma_d[aux1:aux2])
+    end
+    nbetas_Out_Tot = sum(nbetas_Out)
+      
+    #
+    # Updating Matrix M
+    #
+      
+    if(nbetas_In_Tot != nbetas_Out_Tot)  
+      g_d = cumsum(Gamma_d) 
+      M = zeros(Int(nbetas_In_Tot), Int(nbetas_Out_Tot))
+      for i in 1:nbetas_In_Tot
+        if(Gamma_d[i] != 0)
+          M[i, g_d[i]] = 1
+        else
+          M[i, g_d[i-1]] = 1
+        end
+      end
+              
+      # Update W
+      W  =  W * M
+      nbetas_In = nbetas_Out
+      nbetas_In_Tot = sum(nbetas_In)
+      LdP = L_Matrix(1, Int.(nbetas_In))
+      global LdP_i = Inverse_L(Int.(nbetas_In))
+      # Partial solution: solve problem on new dimensions 
+      global HP = XX * W * LdP_i 
+        
+      if (treat_outlier) 
+        # Handlig Outliers
+        global HPo = hcat(HP, Matrix{Float64}(I, Int(n), Int(n)))
+            
+        # Ada LASSO solution with smallest BIC
+        theta_hatP_Est = Ada_LASSO_intercept_outlier(HP, y, HPo,false, false)
+          
+        # Solution part without outliers
+        global theta_hatP = theta_hatP_Est.coef
+        global theta_hatP_itcp = theta_hatP_Est.itcp
+        # BIC value
+        global bic_value = theta_hatP_Est.bic[theta_hatP_Est.bick] 
+
+        # Solution part With Outliers
+        global theta_hatPo = theta_hatP_Est.coefo
+        global theta_hatPo_itcpo = theta_hatP_Est.itcpo
+        # BIC value
+        global bic_value_outliers = theta_hatP_Est.bico[theta_hatP_Est.bicko] 
+          
+        global y_hato = HPo * theta_hatPo           # Without Intercept 
+        global y_hat = HP * theta_hatP              # Without Intercept
+          
+        # No. of Outliers
+        ind_out = findall(pts -> pts > 0, theta_hatPo[(end-n+1):end])  
+                  
+      else
+        theta_hatP_Est = Ada_LASSO_intercept(HP, y, false, false)
+        global theta_hatP = theta_hatP_Est.coef
+        global theta_hatP_itcp = theta_hatP_Est.itcp
+        # BIC value
+        global bic_value = theta_hatP_Est.bic[theta_hatP_Est.bick] 
+        global bic_value_outliers = nothing 
+        
+        global y_hat = HP * theta_hatP # Without Intercept
+        global y_hato = zeros(n) 
+          
+      end # Handle Outliers
+    end # Updating M
+  end  # Time
+
+  # β estimated without outliers
+  v_beta_Par = W * LdP_i * theta_hatP  # Without Intercept
+  m_beta_Par = reshape(v_beta_Par, n, p) 
+
+  # β estimated with outliers
+  if(treat_outlier)                 
+    v_beta_Paro = W * LdP_i * theta_hatPo[1:end-n] # dim(theta_hatPo) is n times (np + n)
+    # Without Intercept
+    m_beta_Paro = reshape(v_beta_Paro, n, p) 
+  else # without outliers => always returns a value for v_beta and m_beta 
+    v_beta_Paro = v_beta_Par
+    m_beta_Paro = m_beta_Par 
+  end
+      
+  # Estimate Intercept
+  β0_hat = mean(y_original) .- mean(XX, dims = 1) * v_beta_Par 
+  
+  # Always consider the estimated intercept, since data was centralized. 
+  #   It need to be descentralized back.
+  y_original_hat = β0_hat .+ y_hat
+  
+  if(treat_outlier)
+    y_original_hat_woutl  = β0_hat .+ y_hato
+  else
+    y_original_hat_woutl =  y_original_hat
+  end
+  
+  # RMSE In Sample
+  RMSE_inS  = sqrt(1/length(y)*sum((y.-y_hat).^2))
+  RMSE_inSo = sqrt(1/length(y)*sum((y.-y_hato).^2))
+
+  # Selected variables X unselected variables, and variables with break 
+  chosen_variables = zeros(p)
+  chosen_variables_break =  zeros(p)
+  chosen_variables_outliers = zeros(p)
+  chosen_variables_outliers_break =  zeros(p)
+  
+  for j in 1:p
+    if (abs.(sum(m_beta_Par[:,j])) > 0)
+      chosen_variables[j] = 1
+      featbk = false
+      βref = m_beta_Par[1,j] # first value of m_beta_Par[:,j] 
+      for k in 1:n
+        if(m_beta_Par[k,j] != βref) # At least one break
+            featbk = true
+        end
+      end
+      if(featbk)
+        chosen_variables_break[j] = 1
+      end
+    end
+    if (abs.(sum(m_beta_Paro[:,j])) > 0)
+      chosen_variables_outliers[j] = 1
+      featbk = false
+      βref = m_beta_Paro[1,j] # first value of m_beta_Par[:,j] 
+      for k in 1:n
+        if(m_beta_Paro[k,j] != βref) # At least one break
+            featbk = true
+        end
+      end
+      if(featbk)
+        chosen_variables_outliers_break[j] = 1
+      end
+    end
+  end
+
+  return (β_np_1 = v_beta_Par , β_n_p = m_beta_Par,
+          βo_np_1 = v_beta_Paro , βo_n_p = m_beta_Paro,
+          β0 = β0_hat, 
+          y_hat =  y_original_hat ,  y_hato = y_original_hat_woutl,
+          mean_x  = mean_x , mean_y  = mean_y, 
+          bic_value = bic_value, bic_value_outliers = bic_value_outliers,            
+          chosen_var = chosen_variables,
+          chosen_var_bk = chosen_variables_break,
+          chosen_var_outliers = chosen_variables_outliers,
+          chosen_var_outliers_bk = chosen_variables_outliers_break,
+          RMSE_inS = RMSE_inS, RMSE_inSo = RMSE_inSo,
+          ind_out = ind_out, IFL_t0 = IFL_t0)
+
+end
---
+++ supporting_functions.jl
@@ -0,0 +1,271 @@
+#
+#  Supporting functions 
+#
+#  AM 22/09/24
+#
+#  Last Update: 19/07/25
+#
+
+#
+# Ldk Matrix
+#
+function Ldk(d::Int, k::Int) 
+  #
+  L_d_k = nothing
+  if(k > d) 
+    # Supporting matrices
+    Z_d_kmd = zeros(d, k-d)
+    Z_kmd_d = zeros(k-d,d)
+    Z_dd    = zeros(d,d)
+    I_kmd   = I(k-d)
+    L_d_k   = I(k) - [Z_d_kmd Z_dd ;I_kmd Z_kmd_d]  
+  else
+    L_d_k  = I(k)
+  end 
+  return L_d_k
+end
+
+#
+# L_Matrix
+#
+function L_Matrix(d::Int64, nbetas::Vector{Int64}) 
+  Ld  = Matrix{Int}(undef, 0, 0)
+  tam  = length(nbetas)
+  list_Ls  = []
+  list_dims = []
+  for i in 1: tam
+    #println("Ldk: d: ",i, " k: ", nbetas[i])
+    Ld_i  = Ldk(d, nbetas[i])
+    #println(Ld_i)
+    push!(list_Ls, Ld_i)
+    if(size(Ld_i) != (1,1))
+      push!(list_dims, size(Ld_i))
+    else
+      push!(list_dims, (1,1))
+    end
+  end
+  
+  for i in 1: tam
+    L_row =  Matrix{Int}(undef, 0, 0)
+    L_left = Matrix{Int}(undef, 0, 0)
+    L_right = Matrix{Int}(undef, 0, 0)
+    if (i > 1) 
+      for left in 1: (i-1)
+        ml = zeros(list_dims[i][1], list_dims[left][2])
+        if (size(L_left, 1) == 0)
+          L_left = ml
+        else
+          L_left = hcat(L_left, ml)
+        end
+      end
+    end
+    if (i < tam) 
+      for right in (i+1):tam
+         mr = zeros(list_dims[i][1], list_dims[right][2]) 
+         if (size(L_right, 1) == 0)  
+          L_right = mr
+        else size(L_right, 1) == size(mr, 1)  # Check if row dimensions match
+            L_right = hcat(L_right, mr)
+        end
+      end
+    end
+    if (size(L_left, 1) == 0)
+      L_row = list_Ls[i]
+    else 
+      L_row = hcat(L_left, list_Ls[i])
+    end
+    
+    if (size(L_right, 1) != 0)
+    #  L_row = list_Ls[i]
+    #else
+      L_row = hcat(L_row,  L_right)
+    end
+    
+    if (size(Ld, 1) == 0)
+      Ld    = L_row
+    else
+      Ld    = vcat(Ld, L_row)
+    end
+  end
+  return Ld
+end
+
+#
+# Inverse_L
+#
+function Inverse_L(nbetas::Vector{Int64}) 
+  Ldi_2  = Matrix{Int}(undef, 0, 0)
+  tam = length(nbetas)
+  for i in 1:tam
+    Block_dim = Int.(nbetas[i])
+    Block_i = LowerTriangular( ones(Block_dim, Block_dim))
+    # Block_row
+    B_row = Matrix{Int}(undef, 0, 0)
+    B_left = Matrix{Int}(undef, 0, 0)
+    B_right = Matrix{Int}(undef, 0, 0)
+
+    if (i > 1) 
+      for left in 1: (i-1)
+        ml = zeros(Block_dim, nbetas[left])
+        if (size(B_left,1) == 0)
+          B_left  = ml
+        else
+          B_left  = hcat(B_left, ml)
+        end
+      end
+    end
+
+    if (i < tam) 
+      for right in (i+1):tam
+        mr = zeros(Block_dim, nbetas[right])
+        if (size(B_right, 1) == 0)
+          B_right = mr
+        else
+          B_right = hcat(B_right, mr)
+        end
+      end
+    end
+
+    if (size(B_left,1) == 0)
+      B_row = Block_i
+    else
+      B_row = hcat(B_left, Block_i)
+    end
+
+    if (size(B_right,1) != 0)
+    #  B_row = B_row, B_right)
+    #else
+      B_row = hcat(B_row, B_right)
+    end
+        
+    if (size(Ldi_2, 1) == 0)
+      Ldi_2 = B_row
+    else
+      Ldi_2 = vcat(Ldi_2, B_row)
+    end
+
+  end
+
+  return Ldi_2
+end
+
+#
+# Bic_GLMNet
+#
+function bic_glmnet(x::Matrix{Float64}, y::Vector{Float64}, alpha::Number; standardize = false, intercept = false, penalty_factor = NaN)
+	"""
+  On glmnet standardize & intercept have default values of TRUE
+  alpha = 1 => LASSO
+  """
+  if abs(alpha - 1) > 1
+    alpha = 1.0
+  end
+  
+  if (any(isnan,penalty_factor))
+		lasso =  glmnet(x, y, standardize=standardize, intercept=intercept, alpha=alpha)
+	else
+    lasso = glmnet(x, y, standardize=standardize, intercept=intercept, alpha=alpha, penalty_factor=penalty_factor)
+  end
+  
+  #lasso =  glmnet(H, y)
+  k  = size(lasso.betas,2) #<- lasso1$dim[2]
+	n  = size(x, 1) #<- lasso1$nobs
+  p  = size(x, 2)  # /n<- lasso1$dim[1]
+  df = 0.0
+
+	logLike = fill(0.0, k, 1) #<- rep(NA, times=k)
+	bic     = fill(0.0, k, 1)#<- rep(NA, times=k)
+	
+	# BIC from Wang et al. (2007): Veiga - Epprecht - Guégan
+	for i in 1:k
+    sigma2  = 0
+	  sigma2  = 1/n * sum((y .- x * lasso.betas[:, i]).^2)
+    df = count( !=(0.0), lasso.betas[:, i])
+    bic[i]  = log(sigma2) + 1/n * log(n) * df
+  end
+
+	# Smallest BIC
+  idx = argmin(bic)[1]
+	bicbeta = lasso.betas[:,idx]
+	biclambda = lasso.lambda[idx]
+	bick = idx #lasso1 
+  bicitcp = lasso.a0[idx]
+  
+	# Considering the Intercept
+	parameters  = lasso.betas 
+    		     
+  return (logLike = logLike, bic = bic, betas = lasso.betas, df = df , lambda = biclambda,
+          beta = bicbeta, bick = bick, parameters= parameters, lambdas = lasso.lambda,
+          itcp =bicitcp)
+	
+end
+
+#
+# Adaptive LASSO wo Outliers
+#
+function Ada_LASSO_intercept(x::Matrix{Float64}, y::Vector{Float64}, standardize = false, intercept = false)
+  
+  # First step: initial Ridge solution  => alpha = 0
+  bic_ridge  = bic_glmnet(x, y, 0.0, standardize=standardize)
+  
+  gamma = 1
+  w = 1 ./abs.(bic_ridge.beta) .^gamma  ## Using gamma = 1
+
+  w .= ifelse.(isinf.(w), 1e4, w)
+  
+  # Ada LASSO solution => alpha = 1
+  bic_adalasso  = bic_glmnet(x, y, 1.0, standardize=false, intercept=intercept, penalty_factor=w)
+
+  # coef1 is theta
+  coef1 = bic_adalasso.beta
+  coef0 = bic_adalasso.itcp  # Intercepto!!
+  
+  return (coef = coef1, parameters = bic_adalasso.parameters, 
+          betas = bic_adalasso.betas, df = bic_adalasso.df, 
+          lambda = bic_adalasso.lambda, 
+          bick = bic_adalasso.bick, bic = bic_adalasso.bic, 
+          itcp = coef0)
+
+end
+
+#
+# Adaptive LASSO with Outliers
+#
+function Ada_LASSO_intercept_outlier(x::Matrix{Float64}, y::Vector{Float64}, x2::Matrix{Float64}, gamma, standardize = false, intercept = false)
+  #  Handles Outliers
+  
+  # First step: initial Ridge solution  => alpha = 0
+  bic_ridge  = bic_glmnet(x, y, 0.0, standardize=standardize)
+  
+  # gamma for Ridge regressions
+  gamma = 1
+
+  w = 1 ./abs.(bic_ridge.beta) .^gamma  ## Using gamma = 1
+  w .= ifelse.(isinf.(w), 1e8, w)
+
+  # Find Outliers Weights
+  res = y - x * bic_ridge.beta
+  wd =  1 ./abs.(res).^gamma
+  wd .= ifelse.(isinf.(wd), 1e8, wd)
+
+  w_extend = vcat(w, wd) 
+  
+  # Ada LASSO solution => alpha = 1
+  bic_adalasso          = bic_glmnet(x, y, 1.0, standardize=false, intercept=intercept, penalty_factor=w)
+  bic_adalasso_outliers = bic_glmnet(x2, y, 1.0, standardize=false, intercept=intercept, penalty_factor=w_extend)
+
+  # coef1 is theta
+  coef1 = bic_adalasso.beta
+  coef0 = bic_adalasso.itcp  # Intercepto!!
+  coef1o = bic_adalasso_outliers.beta
+  coef0o = bic_adalasso_outliers.itcp  # Intercepto!!
+    
+  return (coef = coef1, parameters = bic_adalasso.parameters, 
+          betas = bic_adalasso.betas, df = bic_adalasso.df, 
+          lambda = bic_adalasso.lambda, 
+          bick = bic_adalasso.bick, bic = bic_adalasso.bic,
+          bicko = bic_adalasso_outliers.bick, bico = bic_adalasso_outliers.bic, 
+          itcp = coef0,
+          coefo = coef1o, itcpo = coef0o)
+
+end
